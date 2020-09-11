e830623e473cc676904b560815d0f6c71469a395
TanishqSingla <tanishqsingla00@gmail.com>
2020-09-12 02:22:23 +0530

    rendering dom elements

--- src/app.js
+++ src/app.js
@@ -4,5 +4,11 @@ class ProjectInput {
         this.templateElement = document.getElementById("project-input");
         this.hostElement = document.getElementById("app");
         const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
 }
+const prjInput = new ProjectInput();
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
+  element: HTMLFormElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -12,5 +13,14 @@ class ProjectInput {
       this.templateElement.content,
       true
     );
+
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
+
+const prjInput = new ProjectInput();
